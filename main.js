3ad3843b524319e0e943b564419a8147a825025a
hustlzp <hustlzp@qq.com>
2015-04-29 15:34:21 +0800

    Add upload function to rich editor.

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 $(function () {
     window.richEditor = new Simditor({
         textarea: $('#rich-editor'),
-        toolbar: ['bold', 'italic', 'underline', 'ul', 'ol', 'blockquote', 'code', 'link', 'image', 'markdown']
+        toolbar: ['bold', 'italic', 'underline', 'ul', 'ol', 'blockquote', 'code', 'link', 'image', 'markdown'],
+        defaultImage: 'default.png',
+        upload: {
+            url: '/upload.json',
+            params: null,
+            fileKey: 'file',
+            connectionCount: 1,
+            leaveConfirm: '正在上传文件，如果离开上传会自动取消'
+        }
     });
 
     window.markdownEditor = new Editor({
