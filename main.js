edf044363cb09ed78fb5c2a1352aa755edae53f6
hustlzp <hustlzp@qq.com>
2015-04-30 18:19:40 +0800

    Update example.

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 $(function () {
-    window.richEditor = new Simditor({
-        textarea: $('#rich-editor'),
-        toolbar: ['bold', 'italic', 'underline', 'ul', 'ol', 'blockquote', 'code', 'link', 'image', 'markdown'],
-        defaultImage: 'default.png',
-        toolbarFloat: false,
-        upload: {
-            url: '/XEditor/upload.json',
-            params: null,
-            fileKey: 'file',
-            connectionCount: 1,
-            leaveConfirm: '正在上传文件，如果离开上传会自动取消'
-        }
-    });
+  window.richEditor = new Simditor({
+    textarea: $('#rich-editor'),
+    toolbar: ['bold', 'italic', 'underline', 'ul', 'ol', 'blockquote', 'code', 'link', 'image', 'markdown'],
+    defaultImage: 'default.png',
+    toolbarFloat: false,
+    upload: {
+      url: '/XEditor/upload.json',
+      params: null,
+      fileKey: 'file',
+      connectionCount: 1,
+      leaveConfirm: '正在上传文件，如果离开上传会自动取消'
+    }
+  });
 
-    window.markdownEditor = new Editor({
-        element: document.getElementById('markdown-editor'),
-        status: false
-    });
+  window.markdownEditor = new Editor({
+    element: document.getElementById('markdown-editor'),
+    status: false
+  });
 
-    $('.toolbar-item-markdown').parent().css('float', 'right');
+  $('.toolbar-item-markdown').parent().css('float', 'right');
 });
