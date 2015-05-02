f314d948410ba88600ff1a62ddf8ec2c3d1a5045
hustlzp <hustlzp@qq.com>
2015-05-02 10:06:03 +0800

    Add options to XEditor.

--- src/xeditor.js
+++ src/xeditor.js
@@ -1,18 +1,29 @@
-function XEditor(id) {
+function XEditor(id, options) {
   var $textarea = $('#' + id);
 
   $textarea.hide();
+
+  var options = $.extend({
+    rich: true,
+    url: '',
+    defaultImage: 'default.png',
+    upload: {
+      url: '',
+      fileKey: 'file',
+      params: {}
+    }
+  }, options);
 
   // Rich editor
   this.richEditor = new Simditor({
     textarea: $textarea,
     toolbar: ['bold', 'italic', 'underline', 'ul', 'ol', 'blockquote', 'code', 'link', 'image', 'markdown'],
-    defaultImage: 'default.png',
+    defaultImage: options.defaultImage,
     toolbarFloat: false,
     upload: {
-      url: '/XEditor/upload.json',
-      params: null,
-      fileKey: 'file',
+      url: options.upload.url,
+      params: options.upload.params,
+      fileKey: options.upload.fileKey,
       connectionCount: 1,
       leaveConfirm: '正在上传文件，如果离开上传会自动取消'
     }
@@ -34,7 +45,11 @@ function XEditor(id) {
   // Attach some attr
   this.textarea = $textarea;
   this.element = this.richEditor.el.parent();
-  this.rich = true;
+
+  this.rich = options.rich;
+  if (!this.rich) {
+    this.element.addClass('markdown');
+  }
 }
 
 XEditor.prototype.switchToMarkdown = function () {
